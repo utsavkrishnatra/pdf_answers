9ed848ed9b09a1fc7e86172d145f7b87583ebd86
utsavkk90 <utsavkk90@gmail.com>
2024-06-08 17:57:21 -0400

    solved typscript edge case in api/message/route.ts in case of unathorised user

--- src/app/api/message/route.ts
+++ src/app/api/message/route.ts
@@ -15,6 +15,7 @@ const POST = async (req: NextRequest) => {
 
   const { getUser } = getKindeServerSession();
   const user = await getUser();
+  if (!user) return new Response('Unauthorized', { status: 401 });
   const { id: userId } = user;
 
   if (!userId) return new Response('Unauthorized', { status: 401 });
