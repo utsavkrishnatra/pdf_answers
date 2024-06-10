1e533332897239caf508a34dd40cd7d898bcf650
utsavkk90 <utsavkk90@gmail.com>
2024-06-10 13:41:04 -0400

    modified utils.ts

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -16,8 +16,8 @@ export function absoluteUrl(path: string) {
 }
 
 export function constructMetadata({
-  title = "Quill - the SaaS for students",
-  description = "Quill is an open-source software to make chatting to your PDF files easy.",
+  title = "pdf reads - the SaaS for students",
+  description = "pdf reads is an open-source software to make chatting to your PDF files easy.",
   image = "/thumbnail.png",
   icons = "/favicon.ico",
   noIndex = false
@@ -48,7 +48,7 @@ export function constructMetadata({
       creator: "@joshtriedcoding"
     },
     icons,
-    metadataBase: new URL('https://quill-jet.vercel.app'),
+    metadataBase: new URL('https://pdf-answers.vercel.app'),
     themeColor: '#FFF',
     ...(noIndex && {
       robots: {
